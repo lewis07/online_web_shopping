13634c65f90a54f02d5b6ad510bc93a7a2b67c24
Lewis Bolaton <lewisbolaton@gmail.com>
2019-01-14 03:05:33 +0800

    catalog_form_v02_01

--- online_shopping_iter02/catalog_form.aspx.cs
+++ online_shopping_iter02/catalog_form.aspx.cs
@@ -12,13 +12,14 @@ namespace online_shopping_iter02
     public partial class catalog_form : System.Web.UI.Page
     {
         string[] id = new string[5];
+        string search_string;
 
         protected void Page_Load(object sender, EventArgs e)
         {
             int item_total, page_total, current_page, display_range;
             string search_query;
 
-            //If User is logged in
+            //If User is logged-in
             if (Session["User"] != null)
             {
                 lnk_profile.Visible = true;
@@ -36,107 +37,35 @@ namespace online_shopping_iter02
 
             //Hide no match found
             lbl_no_match.Visible = false;
+
             //If Search Query String exists
             if (Request.QueryString["search"] != null)
-            { 
-                SqlConnection connection = new SqlConnection("Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\\online_shopping_db.mdf;Integrated Security=True;Connect Timeout=30");
-                try
-                {
-                    connection.Open();
-                }
-                catch
-                {
-                    Response.Redirect("error_page.aspx");
-                }
-                search_query = Request.QueryString["search"].Replace("+", " ");
-                SqlCommand command = new SqlCommand(
-                    "SELECT COUNT(*) FROM Products WHERE ProductName LIKE '%" 
-                    + search_query + "%'", connection);
-                command.ExecuteNonQuery();
-                SqlDataReader reader = command.ExecuteReader();
-                reader.Read();
-                item_total = (int)reader[0];
-                connection.Close();
-
-                //If there is at least one match
-                if (item_total > 0)
-                {
-                    page_total = (int)Math.Ceiling((double)item_total / 5);
-                    if (Request.QueryString["page"] != null)
-                        current_page = int.Parse(Request.QueryString["page"]);
-                    else
-                        current_page = 1;
-
-                    if (current_page == 1)
-                        btn_previous.Visible = false;
-                    else
-                        btn_previous.Visible = true;
-
-                    lbl_page.Text = current_page.ToString() + " of " + page_total.ToString();
-
-                    if (current_page == page_total)
-                    {
-                        btn_next.Visible = false;
-                        display_range = item_total % 5;
-                        if (display_range == 0)
-                            display_range = 5;
-                    }
-                    else
-                    {
-                        btn_next.Visible = true;
-                        display_range = 5;
-                    }
-
-
-                }
-                //Else if there is no match found
-                else
-                {
-                    lbl_no_match.Visible = true;
-                    display_range = 0;
-
-                    btn_previous.Visible = false;
-                    btn_next.Visible = false;
-                    lbl_page.Visible = false;
-                }
-
-                if (display_range >= 1)
-                    div_product_01.Visible = true;
-                else
-                    div_product_01.Visible = false;
-
-                if (display_range >= 2)
-                    div_product_02.Visible = true;
-                else
-                    div_product_02.Visible = false;
-
-                if (display_range >= 3)
-                    div_product_03.Visible = true;
-                else
-                    div_product_03.Visible = false;
-
-            }
-            //Else if no search query exists
+                search_string = Request.QueryString["search"].ToString();
             else
-            {
-                //Display all products ordered by ProductId
-                SqlConnection connection = new SqlConnection("Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\\online_shopping_db.mdf;Integrated Security=True;Connect Timeout=30");
-                try
-                {
-                    connection.Open();
-                }
-                catch
-                {
-                    Response.Redirect("error_page.aspx");
-                }
-                SqlCommand command = new SqlCommand("SELECT COUNT(*) FROM Products", 
-                    connection);
-                command.ExecuteNonQuery();
-                SqlDataReader reader = command.ExecuteReader();
-                reader.Read();
-                item_total = (int)reader[0];
-                connection.Close();
+                search_string = "";
 
+            SqlConnection connection = new SqlConnection("Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\\online_shopping_db.mdf;Integrated Security=True;Connect Timeout=30");
+            try
+            {
+                connection.Open();
+            }
+            catch
+            {
+                Response.Redirect("error_page.aspx");
+            }
+            search_query = Request.QueryString["search"].Replace("+", " ");
+            SqlCommand command = new SqlCommand(
+                "SELECT COUNT(*) FROM Products WHERE ProductName LIKE '%"
+                + search_query + "%'", connection);
+            command.ExecuteNonQuery();
+            SqlDataReader reader = command.ExecuteReader();
+            reader.Read();
+            item_total = (int)reader[0];
+            connection.Close();
+
+            //If there is at least one match
+            if (item_total > 0)
+            {
                 page_total = (int)Math.Ceiling((double)item_total / 5);
                 if (Request.QueryString["page"] != null)
                     current_page = int.Parse(Request.QueryString["page"]);
@@ -163,92 +92,33 @@ namespace online_shopping_iter02
                     display_range = 5;
                 }
 
-                connection = new SqlConnection("Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\\online_shopping_db.mdf;Integrated Security=True;Connect Timeout=30");
-                try
-                {
-                    connection.Open();
-                }
-                catch
-                {
-                    Response.Redirect("error_page.aspx");
-                }
-                int first_item = current_page * 1;
-                int fifth_item = current_page * 5;
-                command = new SqlCommand(
-                    "SELECT * " +
-                    "FROM   (   SELECT  ROW_NUMBER() OVER ( ORDER BY ProductId ) AS RowNum, * " +
-                    "           FROM Products ) AS RowConstrainedResult" +
-                    " WHERE RowNum >= " + first_item.ToString() + " AND RowNum <= " + fifth_item.ToString() +
-                    " ORDER BY ProductId", connection);
-                command.ExecuteNonQuery();
-                reader = command.ExecuteReader();
 
-
-                //DISPLAY PRODUCTS
-                {
-                    if (display_range >= 1)
-                    {
-                        reader.Read();
-                        div_product_01.Visible = true;
-                        lnk_product_01.Text = reader["ProductName"].ToString();
-                        lbl_prod_01_price.Text = reader["ProductPrice"].ToString();
-                        id[0] = reader["ProductId"].ToString();
-                        img_prod_01.ImageUrl = id[0];
-                    }
-                    else
-                        div_product_01.Visible = false;
-
-                    if (display_range >= 2)
-                    {
-                        reader.Read();
-                        div_product_02.Visible = true;
-                        lnk_product_02.Text = reader["ProductName"].ToString();
-                        lbl_prod_02_price.Text = reader["ProductPrice"].ToString();
-                        id[1] = reader["ProductId"].ToString();
-                        img_prod_01.ImageUrl = "\\|DataDirectory|\\" + id[1] + ".jpg";
-                    }
-                    else
-                        div_product_02.Visible = false;
-
-                    if (display_range >= 3)
-                    {
-                        reader.Read();
-                        div_product_03.Visible = true;
-                        lnk_product_03.Text = reader["ProductName"].ToString();
-                        lbl_prod_03_price.Text = reader["ProductPrice"].ToString();
-                        id[2] = reader["ProductId"].ToString();
-                        img_prod_01.ImageUrl = "\\|DataDirectory|\\images\\" + id[2] + ".jpg";
-                    }
-                    else
-                        div_product_03.Visible = false;
-
-                    if (display_range >= 4)
-                    {
-                        reader.Read();
-                        div_product_04.Visible = true;
-                        lnk_product_04.Text = reader["ProductName"].ToString();
-                        lbl_prod_04_price.Text = reader["ProductPrice"].ToString();
-                        id[3] = reader["ProductId"].ToString();
-                        img_prod_01.ImageUrl = "\\|DataDirectory|\\images\\" + id[3] + ".jpg";
-                    }
-                    else
-                        div_product_04.Visible = false;
-
-                    if (display_range >= 5)
-                    {
-                        reader.Read();
-                        div_product_05.Visible = true;
-                        lnk_product_05.Text = reader["ProductName"].ToString();
-                        lbl_prod_05_price.Text = reader["ProductPrice"].ToString();
-                        id[4] = reader["ProductId"].ToString();
-                        img_prod_01.ImageUrl = "\\|DataDirectory|\\images\\" + id[4] + ".jpg";
-                    }
-                    else
-                        div_product_03.Visible = false;
-                }
-
-                connection.Close();
             }
+            //Else if there is no match found
+            else
+            {
+                lbl_no_match.Visible = true;
+                display_range = 0;
+
+                btn_previous.Visible = false;
+                btn_next.Visible = false;
+                lbl_page.Visible = false;
+            }
+
+            if (display_range >= 1)
+                div_product_01.Visible = true;
+            else
+                div_product_01.Visible = false;
+
+            if (display_range >= 2)
+                div_product_02.Visible = true;
+            else
+                div_product_02.Visible = false;
+
+            if (display_range >= 3)
+                div_product_03.Visible = true;
+            else
+                div_product_03.Visible = false;
         }
 
         //LINK CLICKS EVENTS
